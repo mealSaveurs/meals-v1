0806e83015dd2e6f1205cb122e7cc9eaf62fd19f
dg80 <dguillin80@gmail.com>
2022-02-06 23:10:38 +0100

    Register page and connect to email and confirm

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,8 +5,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,10 +20,17 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-	        ->add('username')
-            ->add('email')
+	        ->add('username', TextType::class, [
+		        'label' => false,
+		        'attr' => ['placeholder' => 'John']
+	        ])
+            ->add('email', EmailType::class, [
+	            'label' => false,
+	            'attr' => ['placeholder' => 'mail@website.com']
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+	            'label' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
@@ -45,8 +54,8 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-	            'first_options' => ['label' => 'Password'],
-	            'second_options' => ['label' => 'Repeat Password']
+	            'first_options' => ['label' => 'Mot de passe'],
+	            'second_options' => ['label' => 'Confirmation du mot de passe']
             ])
         ;
     }
