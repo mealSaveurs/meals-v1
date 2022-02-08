e3eff9111e439143712f3a8464392abdfe6f95a4
dg80 <dguillin80@gmail.com>
2022-02-08 22:20:02 +0100

    Login page and path route / style css

--- public/assets/js/global.js
+++ public/assets/js/global.js
@@ -5,10 +5,13 @@ function toggleSide() {
     if(sidenav.clientWidth === 0){
         console.log(document.getElementById("sidenav").clientWidth);
         sidenav.style.display = "block";
+        sidenav.style.transitionDuration = "50ms";
+        sidenav.style.opacity = "1";
         sidenav.style.width = "300px";
     }else{
         console.log(document.getElementById("sidenav").clientWidth);
         sidenav.style.width = "0";
+        sidenav.style.opacity = "0";
     }
 }
 
